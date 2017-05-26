1ecd1e756d5f2b9d24845eccdd1c3932a7e4824d
李帅 <shuai19970217@163.com>
2017-05-26 23:33:19 +0800

    5.26作业

--- 第六题/Program.cs
+++ 第六题/Program.cs
@@ -13,14 +13,7 @@ namespace 第六题
             #region 6. 产生0-9之间的随机数作为中奖号，从控制台输入你猜测的中奖号，3次内若有一次猜对，打印输出“恭喜您，中奖啦”，退出游戏，若3次内都没猜对打印”谢谢参与”，也退出游戏。
 
 
-            while (true)
-            {
-                Console.WriteLine("请输入一个数（0--9）：");
-                int num = Convert.ToInt32(Console.ReadLine());
-                GetRandom(num);
-            }
-               
-           
+            GetRandom();    
 
             #endregion
         }
@@ -29,27 +22,36 @@ namespace 第六题
         /// 用于对随机数的判断
         /// </summary>
         /// <param name="num"></param>
-        private static void GetRandom(int num)
+        private static void GetRandom()
         {
-            int i = 0;
+
             Random r = new Random();
             int sj = r.Next(0, 9);
-            for ( i = 0; i < 3; i++)
+            for ( int i = 0; i < 3; i++)
             {
-             
+                Console.WriteLine("请输入一个数（0--9）：");
+                int num = Convert.ToInt32(Console.ReadLine());
                 if (sj == num)
                 {
                     Console.WriteLine("恭喜您，中奖啦");
-                    Environment.Exit(0);
+                    return;
 
+                }
+                else
+                {
+                    if ((2-i)==0)
+                    {
+                        Console.WriteLine("谢谢参与");
+                    }
+                    else
+                    {
+                        Console.WriteLine("谢谢参与");
+                    }
                 } 
                    
             }   
-            Console.WriteLine("谢谢参与");
-            if (i >= 3)
-            {
-                return;
+          
             }
         }
     }
-}
+
